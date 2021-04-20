263e9076908f84cfae389717e204a8f3f15bc583
Pitis <pitis.radu@gmail.com>
2021-04-21 00:09:17 +0300

    docs: updated readme license, contributing guide, code of conduct

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export { diff_html } from './diff'
+import { diff_html } from './diff';
+
+const html1 = '<p>I love this module</p>'
+const html2 = "<p>I love this module, it's awesome</p>"
+
+console.log(diff_html(html1, html2))
