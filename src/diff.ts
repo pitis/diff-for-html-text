49bb4abf24cd29cc634f73451fb3375f35bf3ab3
Pitis <pitis.radu@protonmail.com>
2022-01-12 20:43:04 +0200

    feat: get correct tagTable and edit both diffs

--- src/diff.ts
+++ src/diff.ts
@@ -1,74 +1,24 @@
 import { diff_match_patch as DiffMatchPatch } from 'diff-match-patch'
+import { ITagTable } from './types/ITagTable'
 import { isHTML } from './utils/isHTML'
 import { replaceTags } from './utils/replaceTags'
-
-const startingTags = ['{{0}}', '⚠', '㊗️', '🧵', '⤴️', '⤵️']
-
-export const replaceFromTags = (text: string) => {
-  return text
-    .replace(/<br>/gi, '⚠️')
-    .replace(/<p>/gi, '{{0}}')
-    .replace(/<\/p>/gi, '{{/0}}')
-    .replace(/<h2>/gi, '⚠')
-    .replace(/<\/h2>/gi, '❤')
-    .replace(/<h3>/gi, '㊗️')
-    .replace(/<\/h3>/gi, '㊙️')
-    .replace(/<blockquote>/gi, '🧵')
-    .replace(/<\/blockquote>/gi, '🩸')
-    .replace(/<ol>/gi, '🈸')
-    .replace(/<\/ol>/gi, '🈺')
-    .replace(/<li>/gi, '⤴️')
-    .replace(/<li class="ql-indent-1">/gi, '⤵️')
-    .replace(/<\/li>/gi, '🈳')
-}
-
-export const replaceToTags = (text: string) => {
-  return text
-    .replace(/⚠️/gi, '<br>')
-    .replace(/{{\0}}/gi, '<p>')
-    .replace(/{{\/0}}/gi, '</p>')
-    .replace(/⚠/gi, '<h2>')
-    .replace(/❤/gi, '</h2>')
-    .replace(/㊗️/gi, '<h3>')
-    .replace(/㊙️/gi, '</h3>')
-    .replace(/🧵/gi, '<blockquote>')
-    .replace(/🩸/gi, '</blockquote>')
-    .replace(/🈸/gi, '<ol>')
-    .replace(/🈺/gi, '</ol>')
-    .replace(/⤴️/gi, '<li>')
-    .replace(/⤵️/gi, '<li class="ql-indent-1">')
-    .replace(/🈳/gi, '</li>')
-}
-
-export const replaceWithVersace = (text: string) => {
-  return text
-    .replace(/⚠️/gi, '<br>')
-    .replace(/{{\0}}/gi, '{{0}}<span class="new-text">')
-    .replace(/{{\/0}}/gi, '</span>{{/0}}')
-    .replace(/⚠/gi, '⚠<span class="new-text">')
-    .replace(/❤/gi, '</span>❤')
-    .replace(/㊗️/gi, '㊗️<span class="new-text">')
-    .replace(/㊙️/gi, '</span>㊙️')
-    .replace(/🧵/gi, '🧵<span class="new-text">')
-    .replace(/🩸/gi, '</span>🩸')
-    .replace(/🈸/gi, '🈸')
-    .replace(/🈺/gi, '🈺')
-    .replace(/⤴️/gi, '⤴️<span class="new-text">')
-    .replace(/⤵️/gi, '⤵️<span class="new-text">')
-    .replace(/🈳/gi, '</span>🈳')
-}
 
 export const diff_html = (previous: string, next: string): string | void => {
   if (!isHTML(previous) || !isHTML(next)) {
     return
   }
 
+  let tagTable: ITagTable[] = []
+
   const dmp = new DiffMatchPatch()
 
-  let previousEdited = replaceTags(previous)
-  let nextEdited = replaceTags(next)
+  let previousEdited = replaceTags(previous, tagTable)
+  let nextEdited = replaceTags(next, tagTable)
 
-  const diff = dmp.diff_main(previousEdited, nextEdited)
+  console.log(previousEdited, tagTable)
+  console.log(nextEdited)
 
-  console.log(diff)
+  // const diff = dmp.diff_main(previousEdited.htmlText, nextEdited.htmlText)
+
+  // console.log(diff)
 }
--- src/utils/replaceTags.ts
+++ src/utils/replaceTags.ts
@@ -1,6 +1,7 @@
 import { JSDOM } from 'jsdom'
+import { ITagTable } from '../types/ITagTable'
 
-export function replaceTags(html: string): string {
+export function replaceTags(html: string, tagTable: ITagTable[]) {
   const dom = new JSDOM(`${html}>`)
   let htmlText = ''
 
@@ -12,20 +13,47 @@ export function replaceTags(html: string): string {
   })
 
   dom.window.document.body.querySelectorAll('*').forEach((elem, index) => {
+    // verify if tag already exists in tagTable
+    let exists = true
+
+    if (
+      !tagTable.some(
+        (tag) => `<${elem.tagName.toLowerCase()}>` === tag.htmlElement
+      )
+    ) {
+      exists = false
+      tagTable.push({
+        htmlElement: `<${elem.tagName.toLowerCase()}>`,
+        // id: `{{${index}}}`,
+        id: index,
+      })
+    }
+
     if (htmlText.includes(elem.outerHTML)) {
+      let idx = exists
+        ? tagTable.find(
+            (tag) => `<${elem.tagName.toLowerCase()}>` === tag.htmlElement
+          )?.id
+        : index
+
       htmlText = htmlText
-        .replace(`<${elem.tagName.toLowerCase()}>`, `{{${index}}}`)
-        .replace(`</${elem.tagName.toLowerCase()}>`, `{{/${index}}}`)
+        .replace(`<${elem.tagName.toLowerCase()}>`, `{{${idx}}}`)
+        .replace(`</${elem.tagName.toLowerCase()}>`, `{{/${idx}}}`)
 
       return
     }
 
-    if (!htmlText.includes(elem.outerHTML))
+    if (!htmlText.includes(elem.outerHTML)) {
+      let idx = exists
+        ? tagTable.find(
+            (tag) => `<${elem.tagName.toLowerCase()}>` === tag.htmlElement
+          )?.id
+        : index
       htmlText += elem.outerHTML
-        .replace(`<${elem.tagName.toLowerCase()}>`, `{{${index}}}`)
-        .replace(`</${elem.tagName.toLowerCase()}>`, `{{/${index}}}`)
+        .replace(`<${elem.tagName.toLowerCase()}>`, `{{${idx}}}`)
+        .replace(`</${elem.tagName.toLowerCase()}>`, `{{/${idx}}}`)
+    }
   })
 
-  console.log(htmlText)
   return htmlText
 }
